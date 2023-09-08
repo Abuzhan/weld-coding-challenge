d4adc89a38611dff17f873729687a799648f12c0
Abuzhan Aidaraliyev <abuzhan.aidaraliyev@gmail.com>
2023-09-08 17:15:29 +0200

    Creating controlled logic for worker service

--- apps/worker/src/worker.controller.ts
+++ apps/worker/src/worker.controller.ts
@@ -1,11 +1,31 @@
-import { Controller } from '@nestjs/common';
-import { WorkerService } from './worker.service';
+import {Controller, Logger} from '@nestjs/common';
+import {WorkerService} from './worker.service';
+import {MessagePattern} from '@nestjs/microservices';
+import { StartFetchingCommand } from './data.dto';
 
 @Controller()
 export class WorkerController {
-  constructor(private readonly workerService: WorkerService) {}
+    private readonly logger = new Logger(WorkerController.name);
+    constructor(private readonly workerService: WorkerService) {}
 
-  getHello(): string {
-    return this.workerService.getHello();
-  }
+    @MessagePattern('start-fetching-data')
+    async startFetchingData(data: StartFetchingCommand) {
+        const {intervalInMinutes, processId} = data;
+        this.logger.log(
+            `Starting data fetching with interval ${intervalInMinutes} and process ID ${processId}`
+        );
+        this.workerService.startFetchingData(intervalInMinutes, processId);
+        this.logger.log(
+            `Successfully started data fetching with interval ${intervalInMinutes} minutes`
+        );
+        return 'Data fetching started.';
+    }
+
+    @MessagePattern('stop-fetching-data')
+    async stopFetchingData() {
+        this.logger.log(`Received request to stop data fetching`);
+        this.workerService.stopFetchingData();
+        this.logger.log(`Successfully stopped data fetching`);
+        return 'Data fetching stopped.';
+    }
 }
